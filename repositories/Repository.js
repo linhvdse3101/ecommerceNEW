2134a2cc17348834f0eed6034f1eb33417cce1f8
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-11-11 00:11:20 -0800

    v5

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const baseDomain = 'https://76.76.21.61:443'; // API for products
-export const basePostUrl = 'https://https://76.76.21.61:443'; // API for post
-export const baseStoreURL = 'https://76.76.21.61:443'; // API for vendor(store)
+const baseDomain = 'http://76.76.21.61:443'; // API for products
+export const basePostUrl = 'http://https://76.76.21.61:443'; // API for post
+export const baseStoreURL = 'http://76.76.21.61:443'; // API for vendor(store)
 
 export const customHeaders = {
     Accept: 'application/json',

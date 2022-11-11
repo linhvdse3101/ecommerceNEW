2d4c607f7fe4cd45219e1c127e5f660154ec33d8
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-11-10 23:53:21 -0800

    v3

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const baseDomain = 'http://localhost:1337'; // API for products
-export const basePostUrl = 'http://localhost:1337'; // API for post
-export const baseStoreURL = 'http://localhost:1337'; // API for vendor(store)
+const baseDomain = 'https://76.76.21.61:443'; // API for products
+export const basePostUrl = 'https://https://76.76.21.61:443'; // API for post
+export const baseStoreURL = 'https://76.76.21.61:443'; // API for vendor(store)
 
 export const customHeaders = {
     Accept: 'application/json',

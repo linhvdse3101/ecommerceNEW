0026d350b2acecb226687a5212cd755a38503177
linhvd <linhvdse3101@gmail.com>
2022-11-13 11:31:15 +0700

    v6

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const baseDomain = 'https://https://ecommerceapi-jnhz2sg1b-linhvd3101.vercel.app'; // API for products
-export const basePostUrl = 'https://https://ecommerceapi-jnhz2sg1b-linhvd3101.vercel.app'; // API for post
-export const baseStoreURL = 'https://https://ecommerceapi-jnhz2sg1b-linhvd3101.vercel.app'; // API for vendor(store)
+const baseDomain = 'https://server.ldjvit.com'; // API for products
+export const basePostUrl = 'https://server.ldjvit.com'; // API for post
+export const baseStoreURL = 'https://server.ldjvit.com'; // API for vendor(store)
 
 export const customHeaders = {
     Accept: 'application/json',

266d93e5e9584ba649b3f8a3247d6ff4a4932718
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-11-24 17:24:26 +0700

    change image url

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const baseDomain = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for products
 export const basePostUrl = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for post
 export const baseStoreURL = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for vendor(store)
-const baseImgDomain = 'https://ecommerceapi-kz2w.onrender.com'; // API for products
+const baseImgDomain = 'https://server.ldjvit.com'; // API for products
 
 
 export const customHeaders = {

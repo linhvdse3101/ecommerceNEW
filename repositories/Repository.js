30295b7d63ebcb19fef61ee9539a4af79cdc6d4c
bstardev\linh.vo <linh.vo@bstarsolutions.com>
2022-11-25 14:11:10 +0700

    config deploy

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-// const baseDomain = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for products
-// export const basePostUrl = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for post
-// export const baseStoreURL = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for vendor(store)
+const baseDomain = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for products
+export const basePostUrl = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for post
+export const baseStoreURL = 'https://ecommerceapi-kz2w.onrender.com/api'; // API for vendor(store)
 
-const baseDomain = 'http://localhost:1337/api'; // API for products
-export const basePostUrl = 'http://localhost:1337/api'; // API for post
-export const baseStoreURL = 'http://localhost:1337/api'; // API for vendor(store)
+// const baseDomain = 'http://localhost:1337/api'; // API for products
+// export const basePostUrl = 'http://localhost:1337/api'; // API for post
+// export const baseStoreURL = 'http://localhost:1337/api'; // API for vendor(store)
 // const baseImgDomain = 'https://server.ldjvit.com'; // API for products
 
 
